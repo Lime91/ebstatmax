8056c5cdf8349a77357cf9ec46f5d60314067cf9
Konstantin Emil Thiel <konstantin.thiel@pmu.ac.at>
2022-05-23 17:13:57 +0200

    Refactor nparld documentation (and code)

--- simUtils/R/nparld.R
+++ simUtils/R/nparld.R
@@ -4,8 +4,8 @@
 #' test.
 #' 
 #' `options$target` contains the name of the target variable in data.
-#' `config$first_period_end` is the last point of time that belongs to the first 
-#' study period.
+#' `config$first_period_end` is the last point of time that belongs to the 
+#' first study period.
 #' `config$time_variable` is the name of the variable containing timepoints in 
 #' data.
 #' `config$subject_variable` is the name of the variable that identifies 
@@ -17,7 +17,7 @@
 #' @param options `list` with user-defined command line arguments
 #' @param config `list` with further arguments
 #'
-#' @return the test result (`TRUE` if H0 is rejected, `FALSE` otherwise)
+#' @return `list` of p-values for the respective tests
 #' @export
 nparld <- function(data,
                    options,
@@ -27,17 +27,17 @@ nparld <- function(data,
   period2_data <- data[!query]
   form <- as.formula(paste(
     options$target,
-    paste(config$group_variable, config$time_variable, sep=" * "),
-    sep=" ~ "))
+    paste(config$group_variable, config$time_variable, sep="*"),
+    sep="~"))
   capture.output(
     p_value1 <- nparLD::nparLD(
       form,
       period1_data,
-      subject=config$subject_variable)$ANOVA.test[3,3],
+      subject=config$subject_variable)$ANOVA.test[3, 3],
     p_value2 <- nparLD::nparLD(
       form,
       period2_data,
-      subject=config$subject_variable)$ANOVA.test[3,3]
+      subject=config$subject_variable)$ANOVA.test[3, 3]
   )
   l <- list(
     period_1=(p_value1),
@@ -46,5 +46,3 @@ nparld <- function(data,
   )
   return(l)
 }
-
-
